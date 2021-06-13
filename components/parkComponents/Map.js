f42032af9fca3f02c805651b09f07516e182afc4
konrad <ktalaga90@gmail.com>
2021-06-13 13:48:59 +0100

    merge commit:

--- components/parkComponents/Map.js
+++ components/parkComponents/Map.js
@@ -16,22 +16,20 @@ const Map = ({parks, mapProperty}) => {
         return mapProperty.longitude
       }return -4.0000
     }
-    console.log(newLatitude);
-    
+    const latitudeWorking = newLatitude();
+    const longitudeWorking = newLongitude();
     
     return(
       <MapView
       style={styles.map}
       initialRegion={{
-      latitude: 56.0000,
-      longitude: 16.0000,
+      latitude: {latitudeWorking},
+      longitude: {longitudeWorking},
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     }}
   />
   
-  
-
   );
 }
 const styles = StyleSheet.create({
