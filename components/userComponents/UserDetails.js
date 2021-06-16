c7e55be6a2f2f504ad190c67021eb84135b9188d
Martin Sutherland <77404854+martinsutherland1@users.noreply.github.com>
2021-06-16 15:57:11 +0100

    front complete

--- components/userComponents/UserDetails.js
+++ components/userComponents/UserDetails.js
@@ -8,6 +8,18 @@ const UserDetails = ({currentUser, distanceCovered}) => {
     if (currentUser === null){
         return (null)
     }
+const array = [];
+    for (let i=0; i < currentUser.allRoutes.length; i++){
+        array.push(currentUser.allRoutes[i].routeName)
+    }
+
+    const listItems = array.map((route, index) =>
+  <li index={index}>
+    {route}
+  </li>
+);
+
+    
 
     if(currentUser.totalDistance < currentUser.distanceTarget){
         return (
@@ -19,10 +31,12 @@ const UserDetails = ({currentUser, distanceCovered}) => {
             <Text style={styles.item}>Email: {currentUser.email}</Text>
             <Text style={styles.item}>Known as: {currentUser.alias}</Text>
             <Text style={styles.item}>Monthly Goal: {currentUser.distanceTarget}km</Text>
-            <Text style={styles.itemRed}>Distance Covered: {currentUser.totalDistance}km</Text>
+            <Text style={styles.itemRed}>Distance Covered: {currentUser.totalDistance.toFixed(2)}km</Text>
+            <Text style={styles.itemRoute}>Routes Complete:</Text>
+            <Text style={styles.item}>{listItems}</Text>
+           
             
-
-        </View>
+    </View>
 
         )
     }
@@ -46,6 +60,8 @@ const UserDetails = ({currentUser, distanceCovered}) => {
             <Text style={styles.item}>Known as: {currentUser.alias}</Text>
             <Text style={styles.item}>Monthly Goal: {currentUser.distanceTarget}km</Text>
             <Text style={styles.item}>Distance Covered: {currentUser.totalDistance}km</Text>
+            <Text style={styles.itemRoute}>Routes Complete:</Text>
+            <Text style={styles.item}>{listItems}</Text>
             
 
         </View>
@@ -91,6 +107,13 @@ const UserDetails = ({currentUser, distanceCovered}) => {
             backgroundColor: "red",
             color: '#fff',
             padding: 20,
+            marginVertical: 4,
+            fontSize: 16
+        },
+        itemRoute: {
+            backgroundColor: "#5FA777",
+            color: '#fff',
+            padding: 10,
             marginVertical: 4,
             fontSize: 16
         },
